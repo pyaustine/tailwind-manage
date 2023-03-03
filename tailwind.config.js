62a63e8272b6021618cb3684a15b86a5d3fe5def
Hostine <austinjuniour4@gmail.com>
2023-03-03 15:13:42 +0300

    add custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+
+    },
+    extend: {
+      colors: {
+        brightREd: 'hsl(12, 88%, 59%)',
+        brightREdLight: 'hsl(12, 88%, 69%)',
+        brightREdSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)'
+      }
+    },
   },
   plugins: [],
 }
